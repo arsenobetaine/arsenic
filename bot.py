40dc5a107261bb35a1de98bdcc3e4d916438751d
Sahand Soleimani <arsenobetaine@gmail.com>
2025-05-29 14:43:53 +0330

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,8 +1,9 @@
+import os
 import discord
 from discord.ext import commands
 
 intents = discord.Intents.default()
-intents.message_content = True
+intents.message_content = True  # Required for reading messages
 
 bot = commands.Bot(command_prefix="!", intents=intents)
 
@@ -14,4 +15,4 @@ async def on_ready():
 async def ping(ctx):
     await ctx.send("Pong!")
 
-bot.run("DISCORD_BOT_TOKEN")
+bot.run(os.getenv("DISCORD_BOT_TOKEN"))
