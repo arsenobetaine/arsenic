3e0de3116833ca73af3abbff976b3f172a227c18
Sahand Soleimani <arsenobetaine@gmail.com>
2025-06-28 19:27:46 +0330

    gcloud migration

--- config.py
+++ config.py
@@ -1,8 +1,10 @@
 import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 DISCORD_TOKEN = os.getenv("DISCORD_BOT_TOKEN")
 AUTO_ROLE_ID = int(os.getenv("AUTO_ROLE_ID"))
 PORT = int(os.getenv("PORT", 3000))
 
-# using my test server id during development
-DEV_GUILD_ID = int(os.getenv("DEV_GUILD_ID", 0))  # 0 = global
+DEV_GUILD_ID = int(os.getenv("DEV_GUILD_ID", 0))
