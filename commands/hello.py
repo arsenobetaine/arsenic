0dd2980c6f097fbed7d1f4d453f5a4cbfc1e2e29
Sahand Soleimani <arsenobetaine@gmail.com>
2025-06-28 20:21:38 +0330

    test commit

--- commands/hello.py
+++ commands/hello.py
@@ -1,4 +1,4 @@
 import discord
 
 async def hello_command(interaction: discord.Interaction):
-    await interaction.response.send_message("I'm a femboy kitten UwU!", ephemeral=True)
+    await interaction.response.send_message("I'm a femboy kitten UwU! (psst... I also eat dogs.)", ephemeral=True)
